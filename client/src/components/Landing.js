dbed17716cd10d21ac9b75b54a2fc52d7e8539f1
Liz Mendes <liz_mendes2001@yahoo.co.uk>
2017-08-04 10:49:21 +0100

    Add billing and client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <header style={{textAlign: 'center' }}>
+      <h1>Emaily!</h1>
+      <p>Collect feedback from your users</p>
+    </header>
+  );
+};
+
+export default Landing;
